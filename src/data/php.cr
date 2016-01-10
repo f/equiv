951990d241055487577671ccade764929b2815af
Fatih Kadir Akın <fakin@protel.com.tr>
2016-01-10 16:38:00 +0200

    Add PHP language

---
+++ src/data/php.cr
@@ -0,0 +1,20 @@
+Equiv::Language.add "node", "Node.js", {
+  http_client: [
+    {
+      name:    "guzzle",
+      install: "composer.phar require guzzlehttp/guzzle",
+    },
+  ],
+  web_framework: [
+    {
+      name:    "symfony",
+      install: "sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony",
+    },
+  ],
+  micro_web_framework: [
+    {
+      name:    "silex",
+      install: "composer.phar require silex/silex",
+    },
+  ],
+}
